5b9f11fb0cb1208be6061e1397192c118fb72287
yeongdonge <parrineau@gmail.com>
2022-05-05 17:13:13 +0900

    Feature Repository init

--- src/test/java/toy/loveinassets/LoveinassetsApplicationTests.java
+++ src/test/java/toy/loveinassets/LoveinassetsApplicationTests.java
@@ -1,13 +1,21 @@
 package toy.loveinassets;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Slf4j
 class LoveinassetsApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	static class FeatureYeongdonge {
+		void dongyeongPush() {
+			log.info("동영 푸시");
+		}
 	}
 
 }
