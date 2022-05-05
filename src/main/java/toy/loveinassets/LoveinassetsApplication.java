a9661e6c72c5365668616c74f5d4cecad32ac8ef
leeseonseonje <leeseonje9323@gmail.com>
2022-05-05 17:04:34 +0900

    프로젝트 생성

---
+++ src/main/java/toy/loveinassets/LoveinassetsApplication.java
@@ -0,0 +1,13 @@
+package toy.loveinassets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoveinassetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoveinassetsApplication.class, args);
+	}
+
+}
