4d6bfa9e86cde1a1104ccb0fe268c726eec73124
yeongdonge <parrineau@gmail.com>
2022-05-05 19:02:09 +0900

    Repo init test

--- src/main/java/toy/loveinassets/LoveinassetsApplication.java
+++ src/main/java/toy/loveinassets/LoveinassetsApplication.java
@@ -3,11 +3,11 @@ package toy.loveinassets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class LoveinassetsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoveinassetsApplication.class, args);
 	}
-
 }
